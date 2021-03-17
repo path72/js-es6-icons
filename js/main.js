459546bf5ca6b897e87e95fd52a8a8dea707b76a
Salvatore Trimarchi <path72@gmail.com>
2021-03-17 18:52:37 +0100

    milesotne #3

--- js/main.js
+++ js/main.js
@@ -25,56 +25,35 @@ const itemList = [
 	{ name: 'user-secret',    prefix: 'fa-', type: 'user',      family: 'fas' }
 ];
 
-// * LOCAL COLOR SCHEME */
+// * LOCAL STRUCTURE */
 // default color: position 0 in colorList
 const colorList = ['var(--black)', 'var(--blue)', 'var(--orange)', 'var(--purple)'];
+// html element ids
+const itemDisplayBoxHtmlId = 'item_display_box';
+const typeSelectHtmlId     = 'type_selection';
 
 
 // * ------ * Milestone 1 * ------ *
 // Partendo dalla struttura dati che troviamo sotto, 
 // mostriamo in pagina tutte le icone disponibili come da layout.
 
-// display itemList in itemDisplayBox 
-const itemDisplayBox = $('.item_display_box');
-displayArrayInContainer(itemList,itemDisplayBox);
+// displaying item list in html 
+// displayArrayInContainerById(itemList,itemDisplayBoxHtmlId);
 
 
 // * ------ * Milestone 2 * ------ *
 // Coloriamo le icone per tipo
+
+// defining & displaying colored item list
 const itemColorList = getColoredArray(itemList,colorList);
+displayArrayInContainerById(itemColorList,itemDisplayBoxHtmlId);
 
 
-// ! DEBUG !
-console.log(getTypeList(itemList));
-console.log(getColorByType('animal',getTypeList(itemList),colorList));
-console.log(getColorByType('vegetable',getTypeList(itemList),colorList));
-console.log(getColorByType('user',getTypeList(itemList),colorList));
-console.log(getColorByType('pippo',getTypeList(itemList),colorList));
-console.log(itemColorList);
+// * ------ * Milestone 3 * ------ *
+// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// initializing functions of html select
+selectInit(itemColorList,typeSelectHtmlId,itemDisplayBoxHtmlId,colorList);
 
 
 // *********************** doc ready end ***
@@ -83,12 +62,12 @@ console.log(itemColorList);
 //###################################################### 
 // FUNCTIONS
 
-function displayArrayInContainer(_array,_container) {
-	_container.html('');
+function displayArrayInContainerById(_array,_containerId) {
+	$('#'+_containerId).html('');
 	_array.forEach((el) => {
 		const {family,prefix,name,color} = el;
 		const faStyle = (color != undefined) ? `style="color: ${color}"` : ``; 
-		_container.append(`
+		$('#'+_containerId).append(`
 		<div class="item">
 			<i class="${family} ${prefix}${name}" ${faStyle}"></i>
 			<div class="item_name">${name}</div>
@@ -97,31 +76,30 @@ function displayArrayInContainer(_array,_container) {
 	});
 }
 
-function getColoredArray(_itemList,_colorList) {
+function getColoredArray(_array,_colorList) {
 	/**
-	 * returns array of the elements in _itemList
+	 * returns array of the elements in _array
 	 * where each element gets color property by its type
 	 */
-	const coloredItemList = _itemList.map((el) => {
-		// array of types in _itemList
-		const typeList = getTypeList(_itemList);
-		// color matching type for el
+	const _coloredArray = _array.map((el) => {
+		const typeList = getTypeList(_array);
 		el.color = getColorByType(el.type,typeList,_colorList)
 		return el;
 	});
-	return coloredItemList;
+	return _coloredArray;
 }
 
-function getTypeList(_itemList) { 
+function getTypeList(_array) { 
 	/**
-	 * returns array of types in _itemList
+	 * returns array of types in _array
 	 */
 	const typeList = [];
-	_itemList.forEach((el) => {
+	_array.forEach((el) => {
 		if (!typeList.includes(el.type)) typeList.push(el.type);
 	});
 	return typeList;
 }
+
 function getColorByType(_type,_typeList,_colorList) {
 	/**
 	 * matches _type position in _typeList and
@@ -134,3 +112,23 @@ function getColorByType(_type,_typeList,_colorList) {
 	else return _colorList[0];
 }
 
+function deployTypesInSelectId(_array,_selectId) {
+	const typeList = getTypeList(_array);
+	typeList.forEach((el) => {
+		$('#'+_selectId).append(`<option value="${el}">${el}</option>`);
+	});
+}
+
+function selectInit(_array,_selectId,_containerId,_colorList) {
+	deployTypesInSelectId(_array,_selectId);
+	$('#'+_selectId).change(function() {
+		const selType = $(this).val();
+		let selArray = _array.filter((el) => el.type == selType);
+		if (selArray.length == 0) selArray = _array;
+		displayArrayInContainerById(selArray,_containerId);
+		// matching page colors
+		let col = getColorByType(selType,getTypeList(_array),_colorList);
+		$('.search').css({'background-color':col});
+		$('.logo').css({'color':col});
+	});
+}
